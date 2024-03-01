a3ad0d3c58b101fc83998efd094a1ae929d88354
DesireeCSilva <desiresilva1987@gmail.com>
2024-03-01 12:51:02 +0100

    bmi calculator with TypeScript

---
+++ build/bmi-calculator.js
@@ -0,0 +1,18 @@
+"use strict";
+// console.log("Hello world!")
+function IbmCalculate(altura, peso) {
+    const bmi = peso / (altura ** 2);
+    if (bmi < 25) {
+        return ("Estás muy delgado");
+    }
+    else if (bmi >= 25 && bmi <= 27) {
+        return ("Estás en tu peso");
+    }
+    else {
+        return ("Tienes sobrepeso");
+    }
+}
+console.log(IbmCalculate(1.85, 74));
+console.log(IbmCalculate(1.95, 112));
+console.log(IbmCalculate(1.55, 60));
+console.log(IbmCalculate(1.88, 94));
---
+++ build/main.js
@@ -0,0 +1,86 @@
+"use strict";
+// inferencia
+let texto = "hola mundo";
+texto.math(1);
+let cadenadatexto = "hola mundo";
+cadenadatexto = 2; //error  de tipos
+// inferencia de tipos: sin necesidad de decirle el tipo es capaz de inferirlo y saber que tipo se.
+const PI = 3.14;
+const a = 3;
+let b = a + PI;
+b = "hello"; //gracias a la inferencia nos da error de tipos, no se puede asignar un string a un number.
+// functions
+function saludar(name) {
+    //tipo any de forma implicita.
+    console.log("hola " + name);
+}
+saludar("pepe");
+saludar(2); //error de tipos
+// objetos
+function saludar4({ name: string, age: number }) {
+    // {name, age}: {name: string, age: number}
+    console.log(`hola ${name} tienes ${age} años`);
+}
+// opc 2
+function saludar2(persona) {
+    const { name, age } = persona;
+    console.log(`hola ${name} tienes ${age} años`);
+}
+saludar2({ name: "pepe", age: "pepe" });
+// function return
+function saludar5(persona) {
+    //cambiar string por number, error en el retunr.
+    const { name, age } = persona;
+    console.log(`hola ${name} tienes ${age} años`);
+    return age;
+}
+// Arrow functions
+const sumar = (a, b) => {
+    return a + b;
+};
+const restar = (a, b) => {
+    return a - b;
+};
+//ejemplo de función
+const sayHello = (fn) => {
+    //Error con el void.    OP. 2 (fn: Function)
+    fn("miguel");
+};
+const sayHi = (name) => {
+    console.log(`hola ${name}`);
+};
+sayHello(sayHi);
+// Inferencia en objetos
+let hero = {
+    //let hero: Hero
+    name: "Batman",
+    age: 45,
+    powers: ["Rich", "Intelligent"],
+};
+hero.adress = "Gotham"; //Error, no se puede añadir una propiedad que no está en el objeto.
+// type Hero = {
+//   name: string;
+//   age: number;
+//   powers: string[];
+//   adress: string;    //OP. 2                                               //???????
+// };
+// arrays
+const lenguages = []; //si está vacio, no se puede inferir el tipo.
+// const lenguages:string[] = [];                   //1º opcion
+// const lenguages:Array<string> = [];
+// const lenguages: (string | number | boolean)[] = [];  //2º opcion
+lenguages.push("Java");
+// matrices y tuplas.
+const gameBoard1 = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["O", "O", "X"],
+];
+const gameBoard = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["O", "O", "X"],
+];
+// Extensión Pretty TypeScript Errors
+// **** Investigación: Type Alias, object freeze, mutabilidad, template union types, intersection types,
+// **** type indexing, type from function return.
